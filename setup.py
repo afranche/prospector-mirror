ec34d7d28c6695d7750f04caacba88d6061bf694
Aissaoui Anouar <afranche@protonmail.com>
2019-01-20 00:47:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ from setuptools import setup
 setup(
     name='pre-commit-hooks-prospector-mirror',
     version='0.1.1',
-    install_requires=['prospector>=0.8.0'],
+    install_requires=['prospector>=1.1.6.2', 'mypy>=0.660', 'vulture>=1.0'],
 )
